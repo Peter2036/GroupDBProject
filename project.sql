cf8544189463b68bff2243be2cc40517744882ac
Peter2036 <peterlrose3@gmail.com>
2017-03-27 12:30:45 -0400

    Data capture

--- project.sql
+++ project.sql
@@ -5,6 +5,7 @@ release_year integer,
 Country varchar(25),
 rating		number,
 votes number,
+runtime number,
 primary key(movie_ID));
 
 create table stars(
@@ -26,14 +27,14 @@ writer_id number not null,
 name		varchar(100) not null,
 bday		varchar(25),
 height varchar(25),
-primary key(composer_ID));
+primary key(writer_ID));
 
 create table stars_in(
 movie_ID	number not null,
 star_ID		number not null,
 primary key(movie_ID, star_ID),
 foreign key(movie_ID) references movies(movie_ID),
-foreign key(star_ID) references actors(star_ID));
+foreign key(star_ID) references stars(star_ID));
 
 create table directs(
 movie_ID	number not null,
@@ -47,4 +48,4 @@ movie_ID	number not null,
 writer_ID	number not null,
 primary key(movie_ID, writer_ID),
 foreign key(movie_ID) references movies(movie_ID),
-foreign key(writer_ID) references composers(writer_ID));
+foreign key(writer_ID) references writers(writer_ID));
--- tupleCreator.py
+++ tupleCreator.py
@@ -1,71 +1,74 @@
 from imdb import imdb
+import re
 
 globvarCount = 0
 
 def insertIntoMovies(file, movie_id, title, release_year, country, rating, votes, runtime):
-    file.write("INSERT INTO MOVIES VALUES(", movie_id, ",'", title, "',", release_year, ",'", country, "',", rating, ",", votes, ",", runtime, ");")
+    file.write("INSERT INTO MOVIES VALUES(" + str(movie_id) + ",'" + str(title) + "'," + str(release_year) + ",'" + str(country) + "'," + str(rating) + "," + str(votes) + "," + str(runtime) + ");\n")
     global globvarCount 
     globvarCount = globvarCount + 1
+    print "Record Count: " + str(globvarCount)
     return
 
-def insertIntoStars(file, star_id, name, sex, bday, age):
-    file.write("INSERT INTO STARS VALUES(", star_id, ",'", name, "','", bday, "','", height, "');")
+def insertIntoStars(file, star_id, name, bday, height):
+    file.write("INSERT INTO STARS VALUES(" + str(star_id) + ",'" + str(name) + "','" + str(bday) + "','" + str(height) + "');\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
-def insertIntoDirectors(file, director_id, name, sex, bday, age):
-    file.write("INSERT INTO DIRECTORS VALUES(", director_id, ",'", name, "','", bday, "','", height, "');")
+def insertIntoDirectors(file, director_id, name, bday, height):
+    file.write("INSERT INTO DIRECTORS VALUES(" + str(director_id) + ",'" + str(name) + "','" + str(bday) + "','" + str(height) + "');\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
-def insertIntoWriters(file, composer_id, name, sex, bday, age, rating):
-    file.write("INSERT INTO WRITERS VALUES(", writer_id, ",'", name, "','", bday, "','", height, "');")
+def insertIntoWriters(file, writer_id, name, bday, height):
+    file.write("INSERT INTO WRITERS VALUES(" + str(writer_id) + ",'" + str(name) + "','" + str(bday) + "','" + str(height) + "');\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
 def insertIntoDirects(file, movie_id, director_id):
-    file.write("INSERT INTO DIRECTS VALUES(", movie_id, ",", director_id, ");")
+    file.write("INSERT INTO DIRECTS VALUES(" + str(movie_id) + "," + str(director_id) + ");\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
 def insertIntoStars_in(file, movie_id, star_id):
-    file.write("INSERT INTO STARS_IN VALUES(", movie_id, ",", star_id, ");")
+    file.write("INSERT INTO STARS_IN VALUES(" + str(movie_id) + "," + str(star_id) + ");\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
 def insertIntoWrites_for(file, movie_id, writer_id):
-    file.write("INSERT INTO WRITES_FOR VALUES(", movie_id, ",", writer_id, ");")
+    file.write("INSERT INTO WRITES_FOR VALUES(" + str(movie_id) + "," + str(writer_id) + ");\n")
     global globvarCount
     globvarCount = globvarCount + 1
-    print globvarCount
+    print "Record Count: " + str(globvarCount)
     return
 
 if __name__ == "__main__":
     file = open("tupleRecords.txt", 'w')
     ia = imdb.IMDb(accessSystem = 'http', adultSearch = 0)
-    for i in range(133093, 9999999):
+    for i in range(6144000, 9999999):
         movie = ia.get_movie(str(i))
         ia.update(movie)
+        print "movieID: " + str(i)
         try:
-            if int(movie['year']) >= 1999 and movie['kind'] == 'movie':
+            if int(movie['year']) >= 2010 and movie['kind'] == 'movie':
                 #movie data initialization
                 movie_id = "NULL"
                 title = "NULL"
                 release_year = "NULL"
                 country = "NULL"
-                rating = "NULL"
-                votes = "NULL"
-                runtime = "NULL"
+                rating = 0
+                votes = 0
+                runtime = 0
                 
                 #star data initialization
                 star_id = "NULL"
@@ -87,53 +90,98 @@ if __name__ == "__main__":
 
                 #movie data search
                 movie_id = str(i)
-                title = movie['title']
-                release_year = movie['year']
-                country = movie['country'][0]
-                rating = movie['rating']
-                votes = movie['votes']
-                runtime = movie['runtime']
-
+                title = movie['title'].encode('utf-8')
+                try:
+                    release_year = movie['year']
+                except KeyError:
+                    pass
+                try:
+                    country = movie['country'][0].encode('utf-8')
+                except KeyError:
+                    pass
+                try:
+                    rating = movie['rating']
+                except KeyError:
+                    pass
+                try:
+                    votes = movie['votes']
+                except KeyError:
+                    pass
+                try:
+                    runtime = movie['runtime'][0]
+                    runtime = re.findall('\d+', runtime)[0].encode('utf-8')
+                except KeyError:
+                    pass
                 #movie insert generation
                 insertIntoMovies(file, movie_id, title, release_year, country, rating, votes, runtime)
+                file.flush()
 
                 #star data search
                 for i in movie['cast']:
                     star = ia.get_person(i.getID())
                     star_id = star.getID()
-                    starName = star['name']
-                    starBday = star['birth date']
-                    starHeight = star['height']
-
-                #star insert generation
-                insertIntoStars(file, star_id, starName, starBday, starHeight)
-                insertIntoStars_in(file, movie_id, star_id)
+                    starName = star['name'].encode('utf-8')
+                    try:
+                        starBday = star['birth date'].encode('utf-8')
+                    except KeyError:
+                        starBday = "NULL"
+                        pass
+                    try:
+                        starHeight = star['height'].encode('utf-8')
+                        if "'" in starHeight:
+                            index = starHeight.index("'")
+                            starHeight = starHeight[:index] + "'" + starHeight[index:]
+                    except KeyError:
+                        starHeight = "NULL"
+                        pass
+                    #star insert generation
+                    insertIntoStars(file, star_id, starName, starBday, starHeight)
+                    insertIntoStars_in(file, movie_id, star_id)
+                    file.flush()
 
                 #director data search
                 for i in movie['director']:
                     director = ia.get_person(i.getID())
                     director_id = director.getID()
-                    directorName = director['name']
-                    directorBday = director['birth date']
-                    directorHeight = director['height']
-                
-                #director insert generation
-                insertIntoDirectors(file, director_id, directorName, directorBday, directorHeight)
-                insertIntoDirects(file, movie_id, director_id)
+                    directorName = director['name'].encode('utf-8')
+                    try:
+                        directorBday = director['birth date'].encode('utf-8')
+                    except KeyError:
+                        directorBday = "NULL"
+                        pass
+                    try:
+                        directorHeight = director['height'][0].encode('utf-8')
+                    except KeyError:
+                        directorHeight = "NULL"
+                        pass
+                    #director insert generation
+                    insertIntoDirectors(file, director_id, directorName, directorBday, directorHeight)
+                    insertIntoDirects(file, movie_id, director_id)
+                    file.flush()
                     
                 #writer data search
                 for i in movie['writer']:
                     writer = ia.get_person(i.getID())
                     writer_id = writer.getID()
-                    writerName = writer['name']
-                    writerBday = writer['birth date']
-                    writerHeight = writer['height']
-
-                #writer insert generators    
-                insertIntoWriters(file, writer_id, writerName, writerBday, writerHeight)
-                insertIntoWrites_for(file, movie_id, writer_id)
+                    writerName = writer['name'].encode('utf-8')
+                    try:
+                        writerBday = writer['birth date'].encode('utf-8')
+                    except KeyError:
+                        writerBday = "NULL"
+                        pass
+                    try:
+                        writerHeight = writer['height'].encode('utf-8')
+                    except KeyError:
+                        writerHeight = "NULL"
+                        pass
+                    #writer insert generators    
+                    insertIntoWriters(file, writer_id, writerName, writerBday, writerHeight)
+                    insertIntoWrites_for(file, movie_id, writer_id)
+                    file.flush()
             
         except KeyError:
             pass
-
+        except KeyboardInterrupt:
+            file.flush()
+            file.close()
     file.close()
